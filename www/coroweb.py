67d536c541d414c3ba073d9390975f1c969a5dc5
mf1832199@smail.nju.edu.cn <mf1832199@smail.nju.edu.cn>
2019-03-03 13:32:20 +0800

    inspect获取对象参数信息 未完成...

--- www/coroweb.py
+++ www/coroweb.py
@@ -38,12 +38,41 @@ def post(path):
         wrapper.__route__=path
         return wrapper
     return decorator
-
+#获取各种参数
 def get_required_kw_args(fn):
     args=[]
     params=inspect.signature(fn).parameters
     for name,param in params.items():
         if param.kind==inspect.Parameter.KEYWORD_ONLY and param.default==inspect.Parameter.empty:
+            args.append(name)
+    return tuple(args)
+
+def get_named_kw_args(fn):
+    args=[]
+    params=inspect.signature(fn).parameters
+    for name,param in params.items():
+        if param.kind==inspect.Parameter.KEYWORD_ONLY:
+            args.append(name)
+    return tuple(args)
+
+def has_named_kw_args(fn):
+    #args=[]
+    params=inspect.signature(fn).parameters
+    for name,param in params.items():
+        if param.kind==inspect.Parameter.KEYWORD_ONLY:
+            #args.append(name)
+            return True
+
+def has_var_kw_args(fn):
+    #args=[]
+    params=inspect.signature(fn).parameters
+    for name,param in params.items():
+        if param.kind==inspect.Parameter.VARWORD_ONLY:
+            #args.append(name)
+            return True
+
+
+
 
 
 
