2f598928a910672c510f4dd74e2081fd02106228
mf1832199@smail.nju.edu.cn <mf1832199@smail.nju.edu.cn>
2019-03-04 16:09:50 +0800

    配置文件以及对配置文件遍历等操作

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# @Author:xuqi
+# @time:2019/3/4 15:46
+# @File:config_default.py
+
+configs={
+    'debug':True,
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'www',
+        'password':'www',
+        'db':'xuqi'
+},
+    'session':{
+        'secret':'xuqi'
+    }
+
+}
---
+++ www/config_override.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# @Author:xuqi
+# @time:2019/3/4 15:50
+# @File:config_override.py
+
+configs={
+    'db':{}
+    'host':'127.0.0.1'
+}
